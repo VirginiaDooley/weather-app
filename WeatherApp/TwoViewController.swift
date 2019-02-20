36069a0cdbc1260b6d759b7552e2dd0a804c5e3e
Virginia Dooley <vcdooley@gmail.com>
2019-02-20 20:27:54 +0000

    added outlets and update weather action

---
+++ WeatherApp/TwoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TwoViewController.swift
+//  WeatherApp
+//
+//  Created by Virginia Dooley on 2/20/19.
+//  Copyright © 2019 Virginia Dooley. All rights reserved.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+}
--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var backgroundView: UIImageView!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var updateButton: UIButton!
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        backgroundView.image = UIImage(named: "clearBackground")
+        locationLabel.text = "Los Angeles"
+        statusLabel.text = "In 3 hours"
+        weatherIcon.image = UIImage(named: "clear-night")
+        tempLabel.text = "17º"
+        descriptionLabel.text = "Clear warm night"
+        updateButton.layer.cornerRadius = 20
+        
     }
-
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        statusLabel.text = "Right Now"
+        tempLabel.text = "15º"
+        descriptionLabel.text = "Ready to party"
+    }
 
 }
 
